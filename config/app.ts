2b63f37d06f65aefd65d49bf9363243023e88e21
Daria Nikiforova <darya.nikiforova2013@yandex.ru>
2020-02-23 16:30:40 +0300

    5_1 ia done

--- config/app.ts
+++ config/app.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import database from './database';
+import morgan from 'morgan';
 
 const app: express.Application = express();
 const port = process.env.PORT || 3000;
+
+morgan.token('params', (req: express.Request): string => {
+	const propsArr = [];
+	for (const prop in req.params){
+		propsArr.push(prop);
+	}
+	return propsArr.join();
+});
+
+app.use(morgan(':params :method :url :response-time'));
 
 app.use(express.json());
 app.use('/users', require('../components/User/routers/UserRoute'));
@@ -11,7 +22,7 @@ app.use('/groups', require('../components/Group/routers/GroupRoute'));
 app.listen(port, async () => {
 	try {
 		await database.authenticate();
-		await database.sync({force: true});
+		await database.sync({force: false});
 	} catch (err) {
 		console.log(`Unable to connect to ${port}`);
 	}
