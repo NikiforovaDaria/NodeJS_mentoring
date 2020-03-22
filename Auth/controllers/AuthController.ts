be773808017f9deb99628d4b95cf47c53d373e2a
Daria Nikiforova <darya.nikiforova2013@yandex.ru>
2020-03-22 13:45:42 +0300

    removed extra logic from controller

--- Auth/controllers/AuthController.ts
+++ Auth/controllers/AuthController.ts
@@ -1,24 +1,15 @@
 import { AuthService } from '../services/AuthService';
 import { Request, Response } from 'express';
-import * as jwt from 'jsonwebtoken';
-
-import { privateJwtKey } from '../../config/app';
 
 
 export default class AuthController {
 	public static async loginUser(req: Request, res: Response) {
 		const { login, password } = req.query;
 		try {
-			const addedUser = await AuthService.loginUser(login, password);
-			if (addedUser === null) {
-				return res.status(401).send('Auth failed');
-			} else {
-				const payload = {id: addedUser.id, email: addedUser.login};
-				const token = jwt.sign( payload, privateJwtKey, {expiresIn: '1h'});
-				return res.status(200).json({addedUser, token});
-			}			
+			const token = await AuthService.loginUser(login, password);
+			return res.status(200).json(token);
 		} catch(err) {
-			res.status(500).send('User not found :(');
+			res.status(401).send(err);
 		}
 	}
 }
--- Auth/services/AuthService.ts
+++ Auth/services/AuthService.ts
@@ -1,8 +1,22 @@
+import * as jwt from 'jsonwebtoken';
+
 import { UserDAO } from '../../components/User/data-access/UserDAO';
+import { privateJwtKey } from '../../config/app';
+import { User } from '../../components/User/types/userTypes';
 
 export class AuthService {
 
 	public static async loginUser(login: string, password: string) {
-		return await UserDAO.loginUser(login, password);
+		const loggedUser = await UserDAO.loginUser(login, password);
+		if(loggedUser) {
+			await this.getTokenForLoggedUser(loggedUser);
+		} else {
+			throw ('User not found');
+		}
+	}
+
+	private static async getTokenForLoggedUser(loggedUser: User ) {
+		const payload = {id: loggedUser.id, email: loggedUser.login};
+		return jwt.sign( payload, privateJwtKey, {expiresIn: '1h'});
 	}
 }
